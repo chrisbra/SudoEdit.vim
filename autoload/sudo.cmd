1d1a911ade4d730b1627dacd5f7669c5c5fc1c12
Christian Brabandt <cb@256bit.org>
2012-12-20 14:58:35 +0100

    Batch script for use on Windows

---
+++ autoload/sudo.cmd
@@ -0,0 +1,29 @@
+@echo on
+
+SETLOCAL ENABLEEXTENSIONS
+
+:: File to write to
+set myfile=%1
+set newcontent=%2
+set mode=%3
+set sudo=%4
+shift
+shift
+shift
+shift
+
+:: parameter for runas
+:: Windows cmd.exe is very clumsy to use ;(
+set params=%1
+:loop
+shift
+if [%1]==[] goto afterloop
+set params=%params% %1
+goto loop
+:afterloop
+    
+if %mode% == 'write' (
+    %sudo% %params% "cmd.exe /k type %newcontent% >%myfile%"
+    ) else (
+    %sudo% %params% "cmd.exe /c type %myfile% >%newcontent%"
+    )
