d0bf1c05e55cffc93959b85991d47452d8804ae1
Christian Brabandt <cb@256bit.org>
2013-01-11 13:41:44 +0100

    Better useage on Windows
    sudo.cmd can now make use of UAC if your Windows version supports it.
    You have to set g:sudoAuth to 'uac' to make use of it.
    
    Also better cleanup for Windows.

--- autoload/sudo.cmd
+++ autoload/sudo.cmd
@@ -1,18 +1,27 @@
-@echo on
+@echo off
+cls
 
-SETLOCAL ENABLEEXTENSIONS
+setlocal DisableDelayedExpansion
+set "batchPath=%~0"
+setlocal EnableDelayedExpansion
 
 :: File to write to
-set myfile=%1
-set newcontent=%2
-set mode=%3
-set sudo=%4
+set dummy=%1
+set mode=%2
+set myfile=%3
+set newcontent=%4
+set sudo=%5
+shift
 shift
 shift
 shift
 shift
 
-:: parameter for runas
+if '%sudo%' == 'uac' goto checkPrivileges
+if '%dummy%' == 'ELEV' goto gotPrivileges
+
+:: Use runas or something alike to elevate priviliges, but
+:: first parse parameter for runas
 :: Windows cmd.exe is very clumsy to use ;(
 set params=%1
 :loop
@@ -20,10 +29,49 @@ shift
 if [%1]==[] goto afterloop
 set params=%params% %1
 goto loop
+
 :afterloop
-    
+
+:: Use runas or so to elevate rights
+echo.
+echo ***************************************
+echo Calling %sudo% for Privilege Escalation
+echo ***************************************
 if '%mode%' == 'write' (
     %sudo% %params% "cmd.exe /c type %newcontent% >%myfile%"
     ) else (
     %sudo% %params% "cmd.exe /c type %myfile% >%newcontent%"
     )
+goto end
+
+:: Use UAC to elevate rights, idea taken from:
+:: http://stackoverflow.com/questions/7044985/how-can-i-auto-elevate-my-batch-file-so-that-it-requests-from-uac-admin-rights
+:checkPrivileges
+NET FILE 1>NUL 2>NUL
+if '%errorlevel%' == '0' (goto gotPrivileges) else (goto getPrivileges)
+
+:getPrivileges
+echo.
+echo **************************************
+echo Invoking UAC for Privilege Escalation 
+echo **************************************
+
+set vbs="%temp%\GetPrivileges.vbs"
+echo Set UAC = CreateObject^("Shell.Application"^) > %vbs%
+echo UAC.ShellExecute "!batchPath!", "ELEV !mode! "!myfile!" "!newcontent!""   , "", "runas", 1 >> %vbs%
+%vbs%
+:: Delete dynamic VBScript
+if exist %vbs% del %vbs%
+exit /B 
+
+:gotPrivileges
+::setlocal & pushd .
+if '%mode%' == 'write' (
+    type %newcontent% > %myfile%
+) else (
+    type %myfile% > %newcontent%
+)
+
+if not '%errorlevel%' == 0 echo "An error occured"
+
+:end
